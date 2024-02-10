2b3c7a404298d95e02c34fd268bf60d5cd76aab8
Rayhan Nabi <rayhan.nabi@monstar-lab.com>
2024-02-10 21:46:10 +0600

    Initial Commit

---
+++ Otpio/OtpioApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtpioApp.swift
+//  Otpio
+//
+//  Created by Md. Rayhan Nabi on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct OtpioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
